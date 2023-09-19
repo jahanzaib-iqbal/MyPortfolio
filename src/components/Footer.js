6f3734fdd8945529130f73a39be06ad33a749d07
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-09-19 20:27:19 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Jahanzaib Iqbal</h3>
+          <h3>Made with ❤</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Jahanzaib</h3>
