ae68d0695e0f684d7879c27fe341c2484d3cf41e
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-10-10 01:29:39 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Jahanziab Iqbal.pdf";
+import pdf from "../../Assets/../Assets/Jahanzaib Iqbal.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
